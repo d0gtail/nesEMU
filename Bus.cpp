e390b829338de5b2d276d54e951b33219d28d249
d0gtail <d0gtail@mail.com>
2019-09-18 15:41:22 +0200

    changes to represent read write to the ram by the cpu

--- Bus.cpp
+++ Bus.cpp
@@ -17,16 +17,22 @@ Bus::~Bus() {
 
 void Bus::cpuWrite(uint16_t addr, uint8_t data) {
 
-	// check if address is in the correct range (the complete range for now)
-	if(addr >= 0x000 && addr <=0xFFFF) {
-		cpuRam[addr] = data;
+	/*
+	 * RAM address range. It covers 8kB of but there is only 2kB available
+	 * so these 2kB are 'mirrored' through this address range. Using bitwise AND
+	 * to mask the bottom 11Bits is the same as addr % 2048
+	 */
+	if(addr >= 0x000 && addr <=0x1FFF) {
+		cpuRam[addr & 0x07FF] = data;
 	}
 }
 uint8_t Bus::cpuRead(uint16_t addr, bool bReadOnly) {
 
-	// check if address is in the correct range (the complete range for now)
-	if(addr >= 0x000 && addr <=0xFFFF) {
-		return cpuRam[addr];
+	uint8_t data = 0x00; // temporary for now
+	// check if address is in the correct range (cup addressable range)
+	if(addr >= 0x0000 && addr <=0x1FFF) {
+		// put the read data in the right variable and represent the mirroring every 2048 (0x07FF)
+		data =  cpuRam[addr & 0x07FF];
 	}
-	return 0x00;
+	return data;
 }
